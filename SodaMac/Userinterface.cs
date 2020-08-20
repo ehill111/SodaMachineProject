63ba77d18d52cab1a7cf6d9742c3f75012306ffc
Eric M Hill <ehill111@comcast.net>
2020-08-20 07:01:26 -0400

    Added more Userinterface class Console.WriteLines and Console.ReadLines: (1) Declination of payment and customer subsequent choice, (2) Confirmation of method of payment and prompt for soda flavor and customer subsequent selection, and (3) Confirmation of soda flavor selection, and prompt to not forget change.

--- SodaMac/Userinterface.cs
+++ SodaMac/Userinterface.cs
@@ -9,5 +9,29 @@ namespace SodaMac
     public static class Userinterface
     {
 
+        public static void SelectAndPayForSoda()
+        {
+            Console.WriteLine("Hello! Enter coin, dollar, debit, or credit payment. Push button: (1) Coins, (2) Dollar, (3) Debit, or (4) Credit.");
+            Console.ReadLine();
+            Console.WriteLine("Unfortunately, you are broke. Your card was declined. Choose another method of payment: (1) Coins, (2) Dollar, (3) Debit, or (4) Credit.");
+            Console.WriteLine("You chose to pay with . " + ". What cold soda would you like? (1) Root Beer, (2) Orange, or (3) Cola?");
+            Console.ReadLine();
+            Console.WriteLine("Excellent choice! You chose " + " . Don't forget your change.");
+            Console.ReadLine();
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
