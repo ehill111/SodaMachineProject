ef8749886cc84e4bd8a06fea1570b6a835263c31
Eric M Hill <ehill111@comcast.net>
2020-08-20 12:35:44 -0400

    1. Bug: removed a Console.ReadLine from SelectPaymentForSoda and SelectFlavorForSoda methods. This moved the user to the next prompt. 2. Rearranged all Console.WriteLine and Console.ReadLine of the methods above to the top. This allowed them to work.

--- SodaMac/Userinterface.cs
+++ SodaMac/Userinterface.cs
@@ -16,32 +16,31 @@ namespace SodaMac
         //Methods do something.
         public static string SelectPaymentForSoda()
         {
+            Console.WriteLine("Hello! Enter coin, dollar, debit, or credit payment. Select number and press enter: (1) Coins, (2) Dollar, (3) Debit, or (4) Credit.");
+            Console.ReadLine();
+            Console.WriteLine("Unfortunately, you are broke. Your card was declined. Choose another method of payment and press enter: (1) Coins, or (2) Dollar.");
+            Console.ReadLine();
+            Console.WriteLine("You chose to pay with " +  ". Press enter." );
+            Console.WriteLine("What cold soda would you like? (1) Root Beer, (2) Orange, or(3) Cola? Press enter.");
             List<string> coinListForPrompts = new List<string> { "Coins", "Dollar", "Debit", "Credit", };
             int index = int.Parse(Console.ReadLine()) - 1;
             string moneyIndexChoice;
             moneyIndexChoice = coinListForPrompts[index];
             return coinListForPrompts[index];
             int result = index;
-            Console.WriteLine("Hello! Enter coin, dollar, debit, or credit payment. Push button: (1) Coins, (2) Dollar, (3) Debit, or (4) Credit.");
-            Console.ReadLine();
-            Console.WriteLine("Unfortunately, you are broke. Your card was declined. Choose another method of payment: (1) Coins, (2) Dollar, (3) Debit, or (4) Credit.");
-            Console.ReadLine();
-            Console.WriteLine("You chose to pay with . " + ".");
-
         }
 
         public static string SelectFlavorForSoda()
         {
+            Console.WriteLine("Excellent choice! You chose " + " . Don't forget your change.");
+            Console.ReadLine();
             List<string> canListForPrompts = new List<string> { "Root Beer", "Orange", "Cola" };
             int index = int.Parse(Console.ReadLine()) - 1;
             string canIndexChoice;
             canIndexChoice = canListForPrompts[index];
             return canListForPrompts[index];
             int result = index;
-            Console.WriteLine("What cold soda would you like? (1) Root Beer, (2) Orange, or(3) Cola?");
-            Console.ReadLine();
-            Console.WriteLine("Excellent choice! You chose " + " . Don't forget your change.");
-            Console.ReadLine();
+           
 
         } 
         
